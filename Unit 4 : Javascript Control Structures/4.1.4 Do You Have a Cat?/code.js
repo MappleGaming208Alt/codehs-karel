d8b866d8bd0a096121c01925b0deecee4fa906c8
MappleGaming208Alt <p.handa@share.epsb.ca>
2026-04-14 14:51:53 -0600

    Create code.js

---
+++ Unit 4 : Javascript Control Structures/4.1.4 Do You Have a Cat?/code.js
@@ -0,0 +1,7 @@
+function start() {
+    // Declare a boolean variable - change true/false depending on whether you have a cat
+    var haveCat = true;   // ← Change to false if you don't have a cat
+    
+    // Print the value of the boolean
+    println(haveCat);
+}
